54120dd06c1d474d944034a833db39c3216a4b01
akormous <iamakshatchauhan@gmail.com>
2024-01-14 14:48:16 +0530

    Add info on home page

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -7,6 +7,8 @@ function HomeScreen() {
         <div className="flex flex-col items-center justify-center text-black">
             <h1 className="text-2xl font-semibold">Seat-Select Simulator</h1>
             <p>There is no way you can book the same seat from two users</p>
+            <p>Also, the seats reset whenever there is no activity on the website for a while.</p>
+            <p>You are also given a <b>Reset Seats</b> button to manually reset all seats.</p>
         <Auditorium />
         <Footer />
         </div>
